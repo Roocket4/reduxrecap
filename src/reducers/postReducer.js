fd88d996b4ae8870b613b904598c1d552ba8f995
Zvone <zvone.home@gmail.com>
2019-06-14 15:36:52 +0200

    remaking the app to use redux, and completed it

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POST, NEW_POST } from '../actions/types';
+import { FETCH_POSTS, NEW_POST } from '../actions/types';
 
 
 const initialState = {
@@ -8,6 +8,16 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch(action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload,
+      }
+    case NEW_POST:
+      return {
+        ...state,
+        item: action.payload,
+      }
     default: 
       return state;
   }
